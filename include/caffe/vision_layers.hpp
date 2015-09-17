71575f685f4b96835073acc73825e73443cca1da
Christian Diller <christian.diller@gmx.de>
2015-09-17 22:10:03 +0200

    Added CUDA implementation for SPP_Layer

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -478,6 +478,10 @@ class SPPLayer : public Layer<Dtype> {
       const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
   // calculates the kernel and stride dimensions for the pooling layer,
   // returns a correctly configured LayerParameter for a PoolingLayer
   virtual LayerParameter GetPoolingParam(const int pyramid_level,
