6b25ce9a041c0a4f1621e0a8ff47433b0efeddfa
Christian Diller <mail@christian-diller.de>
2015-10-12 23:06:12 +0200

    SPPLayer Rewrite

--- src/caffe/layers/spp_layer.cpp
+++ src/caffe/layers/spp_layer.cpp
@@ -14,191 +14,140 @@ namespace caffe {
 	using std::max;
 
 	template <typename Dtype>
-	LayerParameter SPPLayer<Dtype>::CreatePoolingParam(const int num_bins, const int bottom_h, const int bottom_w, const SPPParameter spp_param) {
-		LayerParameter pooling_param;
-
-		//Calculate the kernel(window) size and the stride as denoted in the paper for both height and width
-		int kernel_h = ceil(bottom_h / static_cast<double>(num_bins));
-		int stride_h = floor(bottom_h / static_cast<double>(num_bins));
-
-		int kernel_w = ceil(bottom_w / static_cast<double>(num_bins));
-		int stride_w = floor(bottom_w / static_cast<double>(num_bins));
-
-		LOG(INFO) << num_bins << "," << bottom_h << "," << bottom_w << "," << kernel_h << "," << stride_h << "," << kernel_w << "," << stride_w;
-
-		//Set the parameters
-		pooling_param.mutable_pooling_param()->set_kernel_h(kernel_h);
-	 	pooling_param.mutable_pooling_param()->set_kernel_w(kernel_w);
-	 	pooling_param.mutable_pooling_param()->set_stride_h(stride_h);
-		pooling_param.mutable_pooling_param()->set_stride_w(stride_w);
-
-		//Set the pooling method
-		switch (spp_param.pool()) {
-		case SPPParameter_PoolMethod_MAX:
-			pooling_param.mutable_pooling_param()->set_pool(PoolingParameter_PoolMethod_MAX);
-			break;
-		case SPPParameter_PoolMethod_AVE:
-			pooling_param.mutable_pooling_param()->set_pool(PoolingParameter_PoolMethod_AVE);
-			break;
-		case SPPParameter_PoolMethod_STOCHASTIC:
-			pooling_param.mutable_pooling_param()->set_pool(PoolingParameter_PoolMethod_STOCHASTIC);
-			break;
-		default:
-			LOG(FATAL) << "Unknown pooling method.";
-		}
-
-		return pooling_param;
-	}
-
-	template <typename Dtype>
 	void SPPLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+		//Get SPP Parameter
 		const SPPParameter spp_param = this->layer_param_.spp_param();
 
-		//Store parameters as reference values for later reshapes
-		num_ = bottom[0]->num();
-		channels_ = bottom[0]->channels();
-		bottom_h_ = bottom[0]->height();
-		bottom_w_ = bottom[0]->width();
-		reshaped_first_time_ = false;
+    //Store input dimensions
+    num_ = bottom[0]->num();
+    channels_ = bottom[0]->channels();
+    bottom_h_ = bottom[0]->height();
+    bottom_w_ = bottom[0]->width();
 
 		//Check for valid input dimensions
 		CHECK_GT(bottom_h_, 0) << "Input dimensions cannot be zero.";
 		CHECK_GT(bottom_w_, 0) << "Input dimensions cannot be zero.";
 
-		//Support the pyramid type {1,2,3,4} and {1,2,3,6} as used in the paper or 2^i for i = 0..pyramid_height
-		switch (spp_param.pyramid_type()) {
-		case SPPParameter_PyramidType_SPP_NET4:
-			pyramid_bins_.push_back(1); pyramid_bins_.push_back(2); pyramid_bins_.push_back(3); pyramid_bins_.push_back(4);
-			pyramid_height_ = 4;
-			custom_pyramid_ = false;
-			break;
-		case SPPParameter_PyramidType_SPP_NET6:
-			pyramid_bins_.push_back(1); pyramid_bins_.push_back(2); pyramid_bins_.push_back(3); pyramid_bins_.push_back(6);
-			pyramid_height_ = 4;
-			custom_pyramid_ = false;
-			break;
-		case SPPParameter_PyramidType_CUSTOM:
-			pyramid_height_ = spp_param.pyramid_height();
-			custom_pyramid_ = true;
-			break;
-		default:
-			LOG(FATAL) << "Unknown pyramid type.";
+		//Check for valid input parameters
+		CHECK_GT(spp_param.pyramid_shape().size(), 0) << "Spatial Pyramid must have at least one layer.";
+
+		//Set pyramid layers (num_bins per layer) and total number of bins
+		total_num_bins_ = 0;
+		pyramid_height_ = spp_param.pyramid_shape().size();
+		num_bins_w_.resize(pyramid_height_); num_bins_h_.resize(pyramid_height_);
+		LOG(INFO) << "Pyramid shape is";
+		for(int p_layer = 0; p_layer < pyramid_height_; ++p_layer) {
+      CHECK_GT(spp_param.pyramid_shape().Get(p_layer), 0) << "number of bins in pyramid shape must be > 0";
+			num_bins_w_[p_layer] = num_bins_h_[p_layer] = spp_param.pyramid_shape().Get(p_layer);
+			total_num_bins_ += num_bins_w_[p_layer] * num_bins_h_[p_layer];
+			LOG(INFO) << num_bins_w_[p_layer] << "x" << num_bins_h_[p_layer];
 		}
-		CHECK_GT(pyramid_height_, 1) << "Pyramid height must be at least 2.";
-
-		//Clear all vectors
-		split_top_vec_.clear();
-		pooling_bottom_vecs_.clear();
-		pooling_layers_.clear();
-		pooling_top_vecs_.clear();
-		flatten_layers_.clear();
-		flatten_top_vecs_.clear();
-		concat_bottom_vec_.clear();
-
-		// split layer output holders setup
-		for (int i = 0; i < pyramid_height_; i++)
-			split_top_vec_.push_back(new Blob<Dtype>());
-
-		//Setup the split layer which will split the input from bottom to split_top_vec_
-		LayerParameter split_param;
-		split_layer_.reset(new SplitLayer<Dtype>(split_param));
-		split_layer_->SetUp(bottom, split_top_vec_);
-
-		for (int i = 0; i < pyramid_height_; i++) {
-			//If this is a custom pyramid, use 2^i as number of bins; if not, use pre-defined number of bins
-			const int num_bins = custom_pyramid_ ? pow(2, i) : pyramid_bins_[i];
-
-			//# POOLING LAYERS
-			//Create the pooling parameter with number of bins, bottom dimensions and the spp parameter
-			const LayerParameter pooling_param = CreatePoolingParam(num_bins, bottom_h_, bottom_w_, spp_param);
-
-			//Fill the pooling bottom vectors with split layer top vectors
-			pooling_bottom_vecs_.push_back(new vector<Blob<Dtype>*>);
-			pooling_bottom_vecs_[i]->push_back(split_top_vec_[i]);
-
-			//Fill the pooling top vectors into pooling_top_vecs_
-			pooling_top_vecs_.push_back(new vector<Blob<Dtype>*>);
-			pooling_top_vecs_[i]->push_back(new Blob<Dtype>());
-
-			//Setup pooling layers
-			pooling_layers_.push_back(shared_ptr<PoolingLayer<Dtype> > (new PoolingLayer<Dtype>(pooling_param)));
-			pooling_layers_[i]->SetUp(*pooling_bottom_vecs_[i], *pooling_top_vecs_[i]);
-
-			//# FLATTEN LAYERS
-			//Create single flatten output layer and multiple flatten top vectors
-			flatten_top_vecs_.push_back(new vector<Blob<Dtype>*>);
-			flatten_top_vecs_[i]->push_back(new Blob<Dtype>());
-
-			//Setup flatten layers
-			LayerParameter flatten_param;
-			flatten_layers_.push_back(new FlattenLayer<Dtype>(flatten_param));
-			flatten_layers_[i]->SetUp(*pooling_top_vecs_[i], *flatten_top_vecs_[i]);
-
-			//Current flatten top output is a bottom input to concat layer
-			concat_bottom_vec_.push_back((*flatten_top_vecs_[i])[0]);
-		}
-
-		//Setup concat layer
-		LayerParameter concat_param;
-		concat_layer_.reset(new ConcatLayer<Dtype>(concat_param));
-		concat_layer_->SetUp(concat_bottom_vec_, top);
+		LOG(INFO) << "Total number of bins is " << total_num_bins_;
 	}
 
 	template <typename Dtype>
 	void SPPLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-		CHECK_EQ(4, bottom[0]->num_axes()) << "Input must have 4 axes, corresponding to (num, channels, height, width)";
-		// Do nothing if bottom shape is unchanged since last Reshape
-		if (num_ == bottom[0]->num() && channels_ == bottom[0]->channels() &&
-				bottom_h_ == bottom[0]->height() && bottom_w_ == bottom[0]->width() &&
-				reshaped_first_time_) return;
+		CHECK_EQ(4, bottom[0]->num_axes()) << "Input must have 4 axes, "
+		      << "corresponding to (num, channels, height, width)";
 
-		num_ = bottom[0]->num();
+		//Change input dimensions
 		channels_ = bottom[0]->channels();
 		bottom_h_ = bottom[0]->height();
 		bottom_w_ = bottom[0]->width();
-		reshaped_first_time_ = true;
 
-		SPPParameter spp_param = this->layer_param_.spp_param();
-		split_layer_->Reshape(bottom, split_top_vec_);
+		//Reshape max index blob
+		max_idx_.Reshape(num_, channels_, total_num_bins_, 1);
 
-		for (int i = 0; i < pyramid_height_; i++) {
-			int num_bins = custom_pyramid_ ? pow(2, i) : pyramid_bins_[i];
-			LayerParameter pooling_param = CreatePoolingParam(num_bins, bottom_h_, bottom_w_, spp_param);
+		//Top shape passes bottom num and channels and a third dimension, the total number of bins
+		vector<int> top_shape;
+		top_shape.push_back(num_);
+		top_shape.push_back(channels_);
+		top_shape.push_back(total_num_bins_);
 
-			pooling_layers_[i].reset(new PoolingLayer<Dtype>(pooling_param));
-			pooling_layers_[i]->SetUp(*pooling_bottom_vecs_[i], *pooling_top_vecs_[i]);
-			pooling_layers_[i]->Reshape(*pooling_bottom_vecs_[i], *pooling_top_vecs_[i]);
-			flatten_layers_[i]->Reshape(*pooling_top_vecs_[i], *flatten_top_vecs_[i]);
-		}
-		concat_layer_->Reshape(concat_bottom_vec_, top);
+		//Reshape top
+		top[0]->Reshape(top_shape);
 	}
 
 	template <typename Dtype>
 	void SPPLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-		//Split
-		split_layer_->Forward(bottom, split_top_vec_);
-		//Pool and flatten
-		for (int i = 0; i < pyramid_height_; ++i) {
-			pooling_layers_[i]->Forward(*pooling_bottom_vecs_[i], *pooling_top_vecs_[i]);
-			flatten_layers_[i]->Forward(*pooling_top_vecs_[i], *flatten_top_vecs_[i]);
+	  //Store previous pyramid bins
+	  int previous_bins = 0;
+
+		//Loop over pyramid layers
+		for(int p_layer = 0; p_layer < pyramid_height_; ++p_layer) {
+	    //Get top, bottom and mask data
+	    const Dtype* bottom_data = bottom[0]->cpu_data();
+	    Dtype* top_data = top[0]->mutable_cpu_data();
+	    int* mask = max_idx_.mutable_cpu_data();
+	    //Calculate bin width and height
+			Dtype bin_size_w = static_cast<Dtype>(bottom_w_) / static_cast<Dtype>(num_bins_w_[p_layer]);
+			Dtype bin_size_h = static_cast<Dtype>(bottom_h_) / static_cast<Dtype>(num_bins_h_[p_layer]);
+			//Loop over num, channels, bin h and w
+			for (int n = 0; n < num_; ++n) {
+				for (int c = 0; c < channels_; ++c) {
+					for (int nbh = 0; nbh < num_bins_h_[p_layer]; ++nbh) {
+						for (int nbw = 0; nbw < num_bins_w_[p_layer]; ++nbw) {
+						  //Calculate bin start and end coordinates
+              const int wstart = max(static_cast<int>(floor(static_cast<Dtype>(nbw) * bin_size_w)), 0);
+							const int hstart = max(static_cast<int>(floor(static_cast<Dtype>(nbh) * bin_size_h)), 0);
+              const int wend = min(static_cast<int>(ceil(static_cast<Dtype>(nbw + 1) * bin_size_w)), bottom_w_);
+							const int hend = min(static_cast<int>(ceil(static_cast<Dtype>(nbh + 1) * bin_size_h)), bottom_h_);
+							const int bin_index = previous_bins + nbh * num_bins_w_[p_layer] + nbw;
+							//Max pooling within one bin
+							for (int h = hstart; h < hend; ++h) {
+								for (int w = wstart; w < wend; ++w) {
+									const int index = h * bottom_w_ + w;
+									if (bottom_data[index] > top_data[bin_index]) {
+										top_data[bin_index] = bottom_data[index];
+										mask[bin_index] = index;
+									}
+								}
+							}
+						}
+					}
+					//Shift pointers
+          bottom_data += bottom[0]->offset(0, 1);
+          top_data += top[0]->offset(0, 1);
+          mask += top[0]->offset(0, 1);
+				}
+			}
+			previous_bins += num_bins_h_[p_layer] * num_bins_w_[p_layer];
 		}
-		//Concat
-		concat_layer_->Forward(concat_bottom_vec_, top);
 	}
 
 	template <typename Dtype>
 	void SPPLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
 		if (!propagate_down[0]) return;
-		vector<bool> concat_propagate_down(pyramid_height_, true);
-		concat_layer_->Backward(top, concat_propagate_down, concat_bottom_vec_);
-		for (int i = 0; i < pyramid_height_; ++i) {
-			flatten_layers_[i]->Backward(*flatten_top_vecs_[i], propagate_down, *pooling_top_vecs_[i]);
-			pooling_layers_[i]->Backward(*pooling_top_vecs_[i], propagate_down, *pooling_bottom_vecs_[i]);
-		}
 
-		split_layer_->Backward(split_top_vec_, propagate_down, bottom);
-	}
+		//Memset bottom diffs to 0
+    caffe_set(bottom[0]->count(), Dtype(0), bottom[0]->mutable_cpu_diff());
 
+    int previous_bins = 0;
+    //Loop over pyramid layers
+    for(int p_layer = 0; p_layer < pyramid_height_; ++p_layer) {
+      //Get top and bottom diffs
+      const Dtype* top_diff = top[0]->cpu_diff();
+      Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+      const int* mask = max_idx_.cpu_data();
+      //Loop over num, channels, bin h and w
+      for (int n = 0; n < top[0]->num(); ++n) {
+        for (int c = 0; c < channels_; ++c) {
+          for (int nbh = 0; nbh < num_bins_h_[p_layer]; ++nbh) {
+            for (int nbw = 0; nbw < num_bins_w_[p_layer]; ++nbw) {
+              const int index = previous_bins + nbh * num_bins_w_[p_layer] + nbw;
+              const int bottom_index = mask[index];
+              bottom_diff[bottom_index] += top_diff[index];
+            }
+          }
+          //Shift pointers
+          bottom_diff += bottom[0]->offset(0, 1);
+          top_diff += top[0]->offset(0, 1);
+          mask += top[0]->offset(0, 1);
+        }
+      }
+      previous_bins += num_bins_h_[p_layer] * num_bins_w_[p_layer];
+    }
+  }
 
 	INSTANTIATE_CLASS(SPPLayer);
 	REGISTER_LAYER_CLASS(SPP);
--- src/caffe/layers/spp_layer.cu
+++ src/caffe/layers/spp_layer.cu
@@ -1,4 +1,4 @@
-#include <algorithm>
+/*#include <algorithm>
 #include <cfloat>
 #include <vector>
 
@@ -8,30 +8,138 @@
 #include "caffe/util/math_functions.hpp"
 #include "caffe/vision_layers.hpp"
 
+#ifdef __CDT_PARSER__
+#define __global__
+#define CUDA_KERNEL_LOOP(a, b)
+#endif
+
 namespace caffe {
 
-	template <typename Dtype>
-	void SPPLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-		split_layer_->Forward(bottom, split_top_vec_);
-		for (int i = 0; i < pyramid_height_; ++i) {
-			pooling_layers_[i]->Forward(*pooling_bottom_vecs_[i], *pooling_top_vecs_[i]);
-	    	flatten_layers_[i]->Forward(*pooling_top_vecs_[i], *flatten_top_vecs_[i]);
-		}
-		concat_layer_->Forward(concat_bottom_vec_, top);
-	}
+template<typename Dtype>
+__global__
+void SPPForward(const int num_threads,
+  const Dtype* const bottom_data, Dtype* const top_data, int* mask,
+  const int bottom_width, const int bottom_height,
+  const int num_bins_w, const int num_bins_h,
+  const Dtype bin_size_w, const Dtype bin_size_h,
+  const int channels, const int previous_bins) {
+    //Run a CUDA kernel loop
+    CUDA_KERNEL_LOOP(index, num_threads) {
+      //Get current position
+      const int n = index / num_bins_w / num_bins_h / channels;
+      const int c = (index / num_bins_w / num_bins_h) % channels;
+      const int nbh = (index / num_bins_w) % num_bins_h;
+      const int nbw = index % num_bins_w;
+
+      //Calculate start and end
+      int hstart = max(static_cast<int>(floor(static_cast<Dtype>(nbh) * bin_size_h)), 0);
+      int wstart = max(static_cast<int>(floor(static_cast<Dtype>(nbw) * bin_size_w)), 0);
+      int hend = min(static_cast<int>(ceil(static_cast<Dtype>(nbh + 1) * bin_size_h)), bottom_height);
+      int wend = min(static_cast<int>(ceil(static_cast<Dtype>(nbw + 1) * bin_size_w)), bottom_width);
+
+      Dtype maxval = -FLT_MAX;
+      int maxidx = -1;
+      const Dtype* const bottom_slice = bottom_data + (n * channels + c) * bottom_width * bottom_height;
+      for(int h = hstart; h < hend; ++h) {
+        for(int w = wstart; w < wend; ++w) {
+          if(bottom_slice[h * bottom_width + w] > maxval) {
+            maxidx = h * bottom_width + w;
+            maxval = bottom_slice[maxidx];
+          }
+        }
+      }
+      top_data[previous_bins + index] = maxval;
+      mask[previous_bins + index] = maxidx;
+  }
+}
+
+template <typename Dtype>
+void SPPLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  const vector<Blob<Dtype>*>& top) {
+    //Get top, bottom and mask data
+    const Dtype* bottom_data = bottom[0]->gpu_data();
+    Dtype* top_data = top[0]->mutable_gpu_data();
+    int* mask = max_idx_.mutable_gpu_data();
+
+    int previous_bins = 0;
+    //Loop over pyramid layers
+    for(int i = 0; i < pyramid_height_; ++i) {
+      const int count = num_bins_w_[i] * num_bins_h_[i] * num_ * channels_;
+      Dtype bin_size_w = static_cast<Dtype>(bottom_w_) / static_cast<Dtype>(num_bins_w_[p_layer]);
+      Dtype bin_size_h = static_cast<Dtype>(bottom_h_) / static_cast<Dtype>(num_bins_h_[p_layer]);
+
+      SPPForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count,
+        bottom_data, top_data, mask,
+        bottom_w_, bottom_h_,
+        num_bins_w_[i], num_bins_h_[i],
+        bin_size_w, bin_size_h,
+        channels_, previous_bins);
+
+      previous_bins += num_bins_h_[p_layer] * num_bins_w_[p_layer];
+    }
+    CUDA_POST_KERNEL_CHECK;
+}
+
+template <typename Dtype>
+__global__
+void SPPBackward(const int num_threads,
+    const Dtype* const bottom_diff, Dtype* const top_diff, int* mask,
+    const int bottom_width, const int bottom_height,
+    const int num_bins_w, const int num_bins_h,
+    const Dtype bin_size_w, const Dtype bin_size_h,
+    const int channels, const int previous_bins) {
+  //Run a CUDA kernel loop
+  CUDA_KERNEL_LOOP(index, num_threads) {
+    //Get current position
+    const int n = index / bottom_width / bottom_height / channels;
+    const int c = (index / bottom_width / bottom_height) % channels;
+    const int h = (index / bottom_width) % bottom_height;
+    const int w = index % bottom_width;
+
+    const int phstart = (h + pad_h < kernel_h) ? 0 : (h + pad_h - kernel_h) / stride_h + 1;
+    const int phend = min((h + pad_h) / stride_h + 1, pooled_height);
+    const int pwstart = (w + pad_w < kernel_w) ? 0 : (w + pad_w - kernel_w) / stride_w + 1;
+    const int pwend = min((w + pad_w) / stride_w + 1, pooled_width);
+    Dtype gradient = 0;
+    const int offset = (n * channels + c) * bin_size_h * bin_size_w;
+    const Dtype* const top_diff_slice = top_diff + offset;
+    const int* const mask_slice = mask + offset;
+    for (int ph = phstart; ph < phend; ++ph) {
+      for (int pw = pwstart; pw < pwend; ++pw) {
+        if (mask_slice[ph * pooled_width + pw] == h * width + w) {
+          gradient += top_diff_slice[ph * pooled_width + pw];
+        }
+      }
+    }
+    bottom_diff[index] = gradient;
+  }
+}
 	
-	template <typename Dtype>
-	void SPPLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-		if (!propagate_down[0]) return;
-	
-		vector<bool> concat_propagate_down(pyramid_height_, true);
-		concat_layer_->Backward(top, concat_propagate_down, concat_bottom_vec_);
-		for (int i = 0; i < pyramid_height_; ++i) {
-			flatten_layers_[i]->Backward(*flatten_top_vecs_[i], propagate_down, *pooling_top_vecs_[i]);
-	    	pooling_layers_[i]->Backward(*pooling_top_vecs_[i], propagate_down, *pooling_bottom_vecs_[i]);
-		}
-		split_layer_->Backward(split_top_vec_, propagate_down, bottom);
-	}
+template <typename Dtype>
+void SPPLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
+  const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  if (!propagate_down[0]) return;
+  //Get top and bottom diff
+  const Dtype* top_diff = top[0]->gpu_diff();
+  Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
+  const int count = bottom[0]->count();
+  caffe_gpu_set(count, Dtype(0.), bottom_diff);
+  const int* mask = max_idx_.gpu_data();
+
+  int previous_bins = 0;
+  for(int i = 0; i < pyramid_height_; ++i) {
+    SPPBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count,
+        bottom_diff, top_diff, mask,
+        bottom_w_, bottom_h_,
+        num_bins_w_[i], num_bins_h_[i],
+        bin_size_w, bin_size_h,
+        channels_, previous_bins);
+    previous_bins += num_bins_h_[p_layer] * num_bins_w_[p_layer];
+  }
+  CUDA_POST_KERNEL_CHECK;
+}
 	
 	INSTANTIATE_LAYER_GPU_FUNCS(SPPLayer);
-}
+}*/
